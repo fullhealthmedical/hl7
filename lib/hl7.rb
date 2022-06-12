2fb7c4c51cf0f37e733960aa3cb9b7fa472ec1ad
Rodrigo RA <rodrigo.aquino@fullhealthmedical.com>
2022-06-11 21:12:07 -0300

    init

---
+++ lib/hl7.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative "hl7/version"
+
+module HL7
+end
---
+++ spec/hl7_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe HL7 do
+  it "has a version number" do
+    expect(HL7::VERSION).not_to be nil
+  end
+end
